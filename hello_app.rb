86403bbd390a460ba8c3fb48b1b07a6392b4dc63
Nam Nguyen <nam.nguyen.node@gmail.com>
2023-12-13 11:58:34 -0400

    Working with time 🚀

--- hello_app.rb
+++ hello_app.rb
@@ -1,5 +1,9 @@
 require 'sinatra'
 
 get '/' do
-  "hello, world!"
+  DAYNAMES = ["Sunday", "Monday", "Tuesday", "Wednesday",
+              "Thursday", "Friday", "Saturday"]
+
+  dayname = DAYNAMES[Time.now.wday]
+  "Hello, world! Happy #{dayname}."
 end
